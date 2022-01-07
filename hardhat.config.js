c3e65e4d4ad298128f0431e3352095639948f5b3
AbhinavXT <abhinavpathaka17@gmail.com>
2022-01-07 15:21:07 +0530

    Config file changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,18 @@
 require('@nomiclabs/hardhat-waffle')
 require('dotenv').config()
 
+const rinkebyUrl = process.env.ALCHEMY_RINKEBY_URL
+	? process.env.ALCHEMY_RINKEBY_URL
+	: ''
+
+const accountKey = process.env.ACCOUNT_KEY ? process.env.ACCOUNT_KEY : ''
+
 module.exports = {
 	solidity: '0.8.3',
 	networks: {
 		rinkeby: {
-			url: process.env.ALCHEMY_RINKEBY_URL,
-			accounts: [process.env.ACCOUNT_KEY],
+			url: rinkebyUrl,
+			accounts: [accountKey],
 		},
 	},
 }
